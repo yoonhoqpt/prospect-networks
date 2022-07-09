911e3868e49b17021c828f05645a73e962c8a109
Tommy <yoonhoqpt@gmail.com>
2022-07-09 23:07:11 +0800

    changed the uri of the lambda apollo server

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -2,7 +2,7 @@ import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-    uri: "http://localhost:5000/"
+    uri: "https://xy3ojstr30.execute-api.us-west-2.amazonaws.com/beta/"
 });
 
 const authLink = setContext((_, {headers}) => {
