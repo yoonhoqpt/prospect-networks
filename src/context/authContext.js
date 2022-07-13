a38cd463f7f3aef61e87c4bf46920f87f5dc39fe
Tommy <yoonhoqpt@gmail.com>
2022-07-13 13:07:35 +0800

    installed the graphql dependency for fix

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,6 +1,5 @@
 import React, { useReducer, createContext } from 'react';
 import jwtDecode from "jwt-decode"
-import { func } from 'prop-types';
 
 const initialState = {
     user: null
--- src/index.js
+++ src/index.js
@@ -4,17 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import client from './apolloClient';
-
+import { ApolloProvider } from '@apollo/react-hooks';
 import { AuthProvider } from './context/authContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <AuthProvider>
-    
+    <ApolloProvider client={client}>
       <React.StrictMode>
         <App />
       </React.StrictMode>
-    
+    </ApolloProvider>
   </AuthProvider>
 );
 
