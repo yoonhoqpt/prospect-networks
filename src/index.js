823635e2308e2df5c06d7f5c350a2806bcc99d4a
Tommy <yoonhoqpt@gmail.com>
2022-07-13 12:49:55 +0800

    index.js without apolloprovider

--- src/index.js
+++ src/index.js
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import client from './apolloClient';
 
+import { AuthProvider } from './context/authContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
+  <AuthProvider>
+    
       <React.StrictMode>
         <App />
       </React.StrictMode>
     
+  </AuthProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
